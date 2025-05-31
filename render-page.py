fcadbce008a82e2456a2ebad1c45de7bca70cddb
Mutlu Akgun <78053007+MutluRenegado@users.noreply.github.com>
2025-05-31 12:04:31 +0300

    Update render-page.py

--- render-page.py
+++ render-page.py
@@ -1,1 +1,8 @@
+async function loadPage(page) {
+  const html = await window.api.loadPage(page);
+  document.getElementById("banner").innerText = page.toUpperCase() + " PAGE";
+  document.getElementById("content").innerHTML = html;
+}
+
+window.onload = () => loadPage("main");
 
